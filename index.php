4b064b5543e9605032c781ad65f518d2f57a178c
Wiktor Jerzykiewicz <wiktor.jerzykiewicz@uczen.zsk.poznan.pl>
2025-01-07 14:55:07 +0100

    Pierwsze kroczki. To co na lekcji
    Plik nav.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <title>Strona główna</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <nav>
+        <?php include 'nav.php'; showNav('index') ?>
+    </nav>
+
+    <main>
+        <section>
+
+        </section>
+        <section>
+
+        </section>
+        <section>
+
+        </section>
+    </main>
+
+    <footer>
+
+    </footer>
+    
+</body>
+</html>
